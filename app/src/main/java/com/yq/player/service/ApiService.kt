d90c2ce009b99dad6a99a0063bd0ddc692b5df87
87618865 <87618865@qq.com>
2019-11-06 13:38:13 +0800

    add new api

--- app/src/main/java/com/yq/player/service/ApiService.kt
+++ app/src/main/java/com/yq/player/service/ApiService.kt
@@ -1,8 +1,8 @@
 package com.yq.player.service
 
 import com.yq.player.entity.Live
-import com.yq.player.http.api
 import com.yq.player.entity.Tribute
+import com.yq.player.http.api
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,7 +14,12 @@ interface ApiService {
      * @param count: 指定要返回的列表的数据 默认为10
      */
     @GET("races")
-    fun lives(): Call<Tribute<List<Live>>>
+    fun lives(
+        @Query("project") project: String? = null,
+        @Query("league") league: String? = null,
+        @Query("channel") channel: String? = null,
+        @Query("status") status: String? = null
+    ): Call<Tribute<List<Live>>>
 
     @GET("race")
     fun live(@Query("code") code: String): Call<Tribute<Live>>
